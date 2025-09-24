3f96a83120f4d17f1365efd6684b1301b9de5135
iTzFireFoxX <juanarevaloduran@gmail.com>
2025-09-24 03:58:59 -0300

    Dibujado de la serpiente en pantalla y sprites temporales

--- scenes/scripts/mundo.gd
+++ scenes/scripts/mundo.gd
@@ -1,19 +1,19 @@
 extends Node2D
 
 
-var t_mapa: Vector2i = Vector2i(20, 10) # Definir tamaño del mapa (temporal)
-var t_casilla: int = 16 # Definir tamaño de las casillas en pixeles
+var map_size: Vector2i = Vector2i(20, 10) # Definir tamaño del mapa (temporal)
+var cell_size: int = 16 # Definir tamaño de las casillas en pixeles
 
 func _ready():
 	queue_redraw()
 
 func _draw():
 	# Dibujar el mapa en base al tamaño definido anteriormente
-	for ancho in range(t_mapa.x):
-		for alto in range(t_mapa.y):
-			var pos = Vector2i(ancho * t_casilla, alto * t_casilla)
+	for x in range(map_size.x):
+		for y in range(map_size.y):
+			var pos = Vector2i(x * cell_size, y * cell_size)
 			# Dibujar las casillas de manera intercalada
-			if ancho % 2 == alto % 2:
-				draw_rect(Rect2(pos, Vector2i(t_casilla, t_casilla)), Color.WEB_GREEN, true)
+			if x % 2 == y % 2:
+				draw_rect(Rect2(pos, Vector2i(cell_size, cell_size)), Color.WEB_GREEN, true)
 			else:
-				draw_rect(Rect2(pos, Vector2i(t_casilla, t_casilla)), Color.DARK_GREEN, true)
+				draw_rect(Rect2(pos, Vector2i(cell_size, cell_size)), Color.DARK_GREEN, true)
--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -1,20 +1,71 @@
 extends Node2D
 
-var pos : Vector2i = Vector2i(10, 5) # Comenzar en el centro
-var dir : Vector2i = Vector2i.RIGHT # Comenzar moviendo hacia la derecha
-var timer : float = 0.0 # Contar el tiempo
-var move_time : float = 1 # Intervalo de movimiento
 
-@onready var anim = $AnimatedSprite2D
+var timer : float = 0.0 # Contar el tiempo
+var move_time : float = 1.0 # Intervalo de movimiento en segundos
+# Serpiente inicial tamaño 3
+var snake: Array = [
+	[Vector2i(11, 5), Vector2i.RIGHT],
+	[Vector2i(10, 5), Vector2i.RIGHT], 
+	[Vector2i(9, 5), Vector2i.RIGHT]
+]
+
+
+# Definir sprites
+@onready var head_animation = $Head # Temporal (placeholder)
+var body_sprite = preload("res://assets/sprites/snake_body.png") # Temporal (placeholder)
+var corner_sprite = preload("res://assets/sprites/snake_corner.png") # Temporal (placeholder)
+var tail_sprite = preload("res://assets/sprites/snake_tail.png") # Temporal (placeholder)
+
 
 func _ready():
-    position = pos * 16
+	drawSprite()
+	head_animation.play("head_animation") # Animacion de la cabeza de la serpiente
 
 func _process(delta):
-    timer += delta
-    if timer >= move_time:
-        timer = 0.0
-        pos += dir
-        position = pos * 16
+	timer += delta
+	if timer >= move_time:
+		timer = 0.0
+		delSprites()
+		updateSnakePos()
+		drawSprite()
 
-    anim.play("default")
+
+func updateSnakePos(): # Actualiza la posicion en la celda de cada parte de la serpiente sumando su posicion actual con su direccion
+	for i in snake:
+		i[0] += i[1]
+
+func updateSpritePos(sprite, pos): # Actualiza la posicion en la pantalla de un sprite
+	sprite.position = pos * 16
+
+func drawSprite(): # Dibuja los sprites de cada parte de la serpiente en la pantalla
+		for i in range(snake.size()):
+			if i == 0:
+				updateSpritePos(head_animation, snake[0][0])
+				updateSpriteDir(head_animation, snake[0][1])
+			else:
+				var sprite = Sprite2D.new()
+				if i == (snake.size() - 1):
+					sprite.texture = tail_sprite
+				else:
+					sprite.texture = body_sprite
+				sprite.centered = false
+				updateSpritePos(sprite, snake[i][0])
+				updateSpriteDir(sprite, snake[i][1])
+				add_child(sprite)
+
+func updateSpriteDir(sprite, dir): # Actualiza la direccion de un sprite
+	match dir:
+		Vector2i.RIGHT:
+			sprite.rotation_degrees = 0
+		Vector2i.DOWN:
+			sprite.rotation_degrees = 90
+		Vector2i.LEFT:
+			sprite.rotation_degrees = 180
+		Vector2i.UP:
+			sprite.rotation_degrees = -90
+
+func delSprites(): # Borra todos los Sprite2D antiguos
+	for i in get_children():
+		if i is Sprite2D:
+			i.queue_free()
