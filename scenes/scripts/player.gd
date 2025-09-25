347dd9a24576a576006e2b6be6c23f7b0c02788d
iTzFireFoxX <juanarevaloduran@gmail.com>
2025-09-25 03:21:17 -0300

    Sistema de movimiento de la cabeza de la serpiente

--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
 
+# Definir variables
+var dir: Vector2i = Vector2i.RIGHT
 var timer : float = 0.0 # Contar el tiempo
-var move_time : float = 1.0 # Intervalo de movimiento en segundos
+var move_time : float = 0.5 # Intervalo de movimiento en segundos
 # Serpiente inicial tamaño 3
 var snake: Array = [
 	[Vector2i(11, 5), Vector2i.RIGHT],
-	[Vector2i(10, 5), Vector2i.RIGHT], 
+	[Vector2i(10, 5), Vector2i.RIGHT],
 	[Vector2i(9, 5), Vector2i.RIGHT]
 ]
 
@@ -20,12 +22,22 @@ var tail_sprite = preload("res://assets/sprites/snake_tail.png") # Temporal (pla
 
 func _ready():
 	drawSprite()
-	head_animation.play("head_animation") # Animacion de la cabeza de la serpiente
+	head_animation.play("head_animation") # Iniciar animacion de la cabeza de la serpiente
 
 func _process(delta):
+	if Input.is_action_pressed("move_up") and snake[0][1] != Vector2i.DOWN:
+		dir = Vector2i.UP 
+	elif Input.is_action_pressed("move_down") and snake[0][1] != Vector2i.UP:
+		dir = Vector2i.DOWN
+	elif Input.is_action_pressed("move_right") and snake[0][1] != Vector2i.LEFT:
+		dir = Vector2i.RIGHT
+	elif Input.is_action_pressed("move_left") and snake[0][1] != Vector2i.RIGHT:
+		dir = Vector2i.LEFT
+
 	timer += delta
 	if timer >= move_time:
 		timer = 0.0
+		snake[0][1] = dir
 		delSprites()
 		updateSnakePos()
 		drawSprite()
@@ -36,34 +48,34 @@ func updateSnakePos(): # Actualiza la posicion en la celda de cada parte de la s
 		i[0] += i[1]
 
 func updateSpritePos(sprite, pos): # Actualiza la posicion en la pantalla de un sprite
-	sprite.position = pos * 16
+	sprite.position = (pos * 16) + Vector2i(8, 8)
 
 func drawSprite(): # Dibuja los sprites de cada parte de la serpiente en la pantalla
 		for i in range(snake.size()):
 			if i == 0:
 				updateSpritePos(head_animation, snake[0][0])
-				updateSpriteDir(head_animation, snake[0][1])
+				updateSpriteRotation(head_animation, snake[0][1])
 			else:
 				var sprite = Sprite2D.new()
 				if i == (snake.size() - 1):
 					sprite.texture = tail_sprite
 				else:
 					sprite.texture = body_sprite
-				sprite.centered = false
+				sprite.centered = true
 				updateSpritePos(sprite, snake[i][0])
-				updateSpriteDir(sprite, snake[i][1])
-				add_child(sprite)
+				updateSpriteRotation(sprite, snake[i][1])
+				add_child(sprite) # Añadir sprite como nodo hijo
 
-func updateSpriteDir(sprite, dir): # Actualiza la direccion de un sprite
-	match dir:
-		Vector2i.RIGHT:
-			sprite.rotation_degrees = 0
-		Vector2i.DOWN:
-			sprite.rotation_degrees = 90
-		Vector2i.LEFT:
-			sprite.rotation_degrees = 180
+func updateSpriteRotation(sprite, d): # Actualiza la direccion de un sprite
+	match d:
 		Vector2i.UP:
 			sprite.rotation_degrees = -90
+		Vector2i.DOWN:
+			sprite.rotation_degrees = 90
+		Vector2i.RIGHT:
+			sprite.rotation_degrees = 0
+		Vector2i.LEFT:
+			sprite.rotation_degrees = 180
 
 func delSprites(): # Borra todos los Sprite2D antiguos
 	for i in get_children():
