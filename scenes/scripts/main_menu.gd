80da1da44a9db5ec2d967651b2ad9570eb561143
iTzFireFoxX <juanarevaloduran@gmail.com>
2026-03-07 10:02:28 -0300

    Rediseño del menu principal

--- scenes/scripts/main_menu.gd
+++ scenes/scripts/main_menu.gd
@@ -15,14 +15,12 @@ func _on_play_pressed() -> void:
 	self.visible = false
 	get_tree().change_scene_to_file("res://scenes/Game.tscn")
 
-
-func _on_exit_pressed() -> void:
-	get_tree().quit()
-
-
 func _on_options_pressed() -> void:
 	title.visible = false
 	version.visible = false
 
 	main_buttons.visible = false
 	options_menu.visible = true
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
