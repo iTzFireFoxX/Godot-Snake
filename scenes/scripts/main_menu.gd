012f506f1dd028d09c0a0faa302dcfa82682a767
iTzFireFoxX <juanarevaloduran@gmail.com>
2026-03-07 11:12:01 -0300

    v0.1.1-beta

--- scenes/scripts/main_menu.gd
+++ scenes/scripts/main_menu.gd
@@ -1,11 +1,6 @@
 extends Control
 
-
-@onready var title = $Title
-@onready var main_buttons = $MainButtons
 @onready var version = $Version
-@onready var play_menu = $PlayMenu
-@onready var options_menu = $OptionsMenu
 
 
 func _ready():
@@ -15,12 +10,14 @@ func _on_play_pressed() -> void:
 	self.visible = false
 	get_tree().change_scene_to_file("res://scenes/Game.tscn")
 
+'''
 func _on_options_pressed() -> void:
 	title.visible = false
 	version.visible = false
 
 	main_buttons.visible = false
 	options_menu.visible = true
+'''
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
