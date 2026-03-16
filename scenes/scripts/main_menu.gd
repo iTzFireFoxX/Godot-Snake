0c29ca43a1b52e572a33b471bb722ba2e19c5ef2
iTzFireFoxX <juanarevaloduran@gmail.com>
2026-03-16 14:51:36 -0300

    Reorganizacion nodo MainMenu

--- scenes/scripts/main_menu.gd
+++ scenes/scripts/main_menu.gd
@@ -1,23 +1,25 @@
 extends Control
 
-@onready var version = $Version
+@onready var version = $Version # Nodo label para mostrar la versión actual
+@onready var main_menu_control = $MainMenuARContainer/MainMenuControl # Main menu
+@onready var play_menu_control = $MainMenuARContainer/PlayMenuControl # Play menu
+@onready var options_menu_control = $MainMenuARContainer/OptionsMenuControl # Options menu
 
 
 func _ready():
-	version.text = "v" + str(ProjectSettings.get("application/config/version"))
+	version.text = "v" + str(ProjectSettings.get("application/config/version")) # Obtener la versión actual del proyecto
 
-func _on_play_pressed() -> void:
+func _on_play_pressed() -> void: # Reworkear menu de jugar
 	self.visible = false
 	get_tree().change_scene_to_file("res://scenes/Game.tscn")
 
-'''
+
 func _on_options_pressed() -> void:
-	title.visible = false
+	main_menu_control.visible = false
 	version.visible = false
 
-	main_buttons.visible = false
-	options_menu.visible = true
-'''
+	options_menu_control.visible = true
 
-func _on_exit_pressed() -> void:
+
+func _on_exit_pressed() -> void: # Salir
 	get_tree().quit()
