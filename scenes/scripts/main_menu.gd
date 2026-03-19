284dc6656b6bb80ae83378eb8b01a8dd127fc477
iTzFireFoxX <juanarevaloduran@gmail.com>
2026-03-19 14:07:14 -0300

    Base para menu de opciones y config del juego

--- scenes/scripts/main_menu.gd
+++ scenes/scripts/main_menu.gd
@@ -9,17 +9,28 @@ extends Control
 func _ready():
 	version.text = "v" + str(ProjectSettings.get("application/config/version")) # Obtener la versión actual del proyecto
 
-func _on_play_pressed() -> void: # Reworkear menu de jugar
-	self.visible = false
-	get_tree().change_scene_to_file("res://scenes/Game.tscn")
-
-
-func _on_options_pressed() -> void:
+func _on_play_config_pressed() -> void: # Menu de jugar
 	main_menu_control.visible = false
 	version.visible = false
+	play_menu_control.visible = true
 
+
+func _on_options_pressed() -> void: # Menu de opciones
+	main_menu_control.visible = false
+	version.visible = false
 	options_menu_control.visible = true
 
 
 func _on_exit_pressed() -> void: # Salir
 	get_tree().quit()
+
+
+func _on_back_button_pressed() -> void: # Boton de volver
+	options_menu_control.visible = false
+	play_menu_control.visible = false
+	main_menu_control.visible = true
+
+
+func _on_play_button_pressed() -> void: # Boton de jugar
+	self.visible = false
+	get_tree().change_scene_to_file("res://scenes/Game.tscn")
